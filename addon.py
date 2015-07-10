e9d4d9f67aa892fc007d5a262897cf48e9fb0fed
Florian Bury <mail@florian-bury.de>
2015-07-10 21:33:22 +0200

    iT CREATES A LIST NOW!!!

--- addon.py
+++ addon.py
@@ -1,3 +1,38 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+import xbmcaddon
+import xbmcgui
 
+addon = xbmcaddon.Addon();
+addonname = addon.getAddonInfo("name");
+
+def createListing():
+ """
+ Creates a listing that XBMC can display as a directory listing
+ @return list
+ """
+ listing = []
+ listing.append('The first item')
+ listing.append('The second item')
+ listing.append('The third item')
+ listing.append('The fourth item')
+ return listing
+
+
+def sendToXbmc(listing):
+     """
+     Sends a listing to XBMC for display as a directory listing
+     Plugins always result in a listing
+     @param list listing
+     @return void
+     """
+     #access global plugin id
+     global thisPlugin
+     # send each item to xbmc
+     for item in listing:
+     listItem = xbmcgui.ListItem(item)
+     xbmcplugin.addDirectoryItem(thisPlugin,'',listItem)
+     # tell xbmc we have finished creating the directory listing
+     xbmcplugin.endOfDirectory(thisPlugin)
+
+     
+# Step 3 - run the program
+sendToXbmc(createListing())
