adf999304a1efe3f70704305d6ef4ef2fa9a6626
Florian Bury <mail@florian-bury.de>
2015-07-12 11:10:28 +0200

    Add Settings Can Parse JSON-File Now

--- addon.py
+++ addon.py
@@ -1,38 +1,39 @@
 import xbmcaddon
 import xbmcgui
+import xbmcplugin
+import sys
+import json
+
+# magic; id of this plugin - cast to integer
+thisPlugin = int(sys.argv[1])
 
 addon = xbmcaddon.Addon();
 addonname = addon.getAddonInfo("name");
 
 def createListing():
- """
- Creates a listing that XBMC can display as a directory listing
- @return list
- """
- listing = []
- listing.append('The first item')
- listing.append('The second item')
- listing.append('The third item')
- listing.append('The fourth item')
- return listing
+        listing = []
+        listing.append('The first item')
+        listing.append('The second item')
+        listing.append('The third item')
+        listing.append('The fourth item')
+        return listing
 
 
-def sendToXbmc(listing):
-     """
-     Sends a listing to XBMC for display as a directory listing
-     Plugins always result in a listing
-     @param list listing
-     @return void
-     """
-     #access global plugin id
-     global thisPlugin
-     # send each item to xbmc
-     for item in listing:
-     listItem = xbmcgui.ListItem(item)
-     xbmcplugin.addDirectoryItem(thisPlugin,'',listItem)
-     # tell xbmc we have finished creating the directory listing
-     xbmcplugin.endOfDirectory(thisPlugin)
+def sendToXbmc():
+        global thisPlugin
 
-     
+        with open('data.json') as dataFile:
+                data = json.load(dataFile);
+
+        for Game in data['Games']:
+                listItem = xbmcgui.ListItem(Game['name'])
+                listItem.setArt({"banner": Game['banner'], "clearart": Game['clearart'], "clearlogo": Game['clearlogo'], "poster": Game['poster'], "thumbnail": Game['poster']})
+                listItem.setLabel(Game['name'])
+                listItem.setThumbnailImage(Game['poster'])
+                xbmcplugin.addDirectoryItem(thisPlugin,'',listItem)
+        xbmcplugin.endOfDirectory(thisPlugin)
+
+
 # Step 3 - run the program
-sendToXbmc(createListing())
+sendToXbmc()
+
