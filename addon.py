db0186c3ba2c4fb657a1d554cf72dd40b2442eb7
Florian Bury <mail@florian-bury.de>
2015-07-15 11:05:34 +0200

    Added Settings Reads Output from FusionCLI NO ERRORHANDLIG FOR NOW

--- addon.py
+++ addon.py
@@ -1,39 +1,76 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 import xbmcaddon
 import xbmcgui
 import xbmcplugin
 import sys
 import json
+import urllib
+import urllib2
+import subprocess
 
 # magic; id of this plugin - cast to integer
 thisPlugin = int(sys.argv[1])
 
 addon = xbmcaddon.Addon();
 addonname = addon.getAddonInfo("name");
-#check
-def createListing():
-        listing = []
-        listing.append('The first item')
-        listing.append('The second item')
-        listing.append('The third item')
-        listing.append('The fourth item')
-        return listing
+addonID = addon.getAddonInfo('id')
+
+fusionDir = addon.getSetting("cwd")
+fusionCLI = fusionDir + "FusionCLI"
 
 
-def sendToXbmc():
+
+def launchGame(gameID):
+        param = '--launch ' + gameID
+        xbmcgui.Dialog().ok(addonname, param, fusionCLI, fusionDir);
+        subprocess.Popen([fusionCLI, param],cwd=fusionDir, shell=True);
+
+def parameters_string_to_dict(parameters):
+    paramDict = {}
+    if parameters:
+        paramPairs = parameters[1:].split("&")
+        for paramsPair in paramPairs:
+            paramSplits = paramsPair.split('=')
+            if (len(paramSplits)) == 2:
+                paramDict[paramSplits[0]] = paramSplits[1]
+    return paramDict
+
+def showAllGames():
         global thisPlugin
+        dataFile = ""
+        param = '--allGames'
+        cli =  subprocess.Popen([fusionCLI, param],cwd=fusionDir, shell=True, stdout=subprocess.PIPE)
+        while True:
+          line = cli.stdout.readline()
+          if len(line)>=1:
+                dataFile += line.rstrip()
+          else:
+                break
 
-        with open('data.json') as dataFile:
-                data = json.load(dataFile);
+        data = json.loads(dataFile);
 
         for Game in data['Games']:
                 listItem = xbmcgui.ListItem(Game['name'])
                 listItem.setArt({"banner": Game['banner'], "clearart": Game['clearart'], "clearlogo": Game['clearlogo'], "poster": Game['poster'], "thumbnail": Game['poster']})
-                listItem.setLabel(Game['name'])
+
+                p= 'plugin://'+addonID+'/?action=launch&id='+Game['id']
+                u = 'RunPlugin('+p+')'
+
+                listItem.addContextMenuItems([("Launch", u,)])
+
                 listItem.setThumbnailImage(Game['poster'])
-                xbmcplugin.addDirectoryItem(thisPlugin,'',listItem)
+                xbmcplugin.addDirectoryItem(thisPlugin,p,listItem)
         xbmcplugin.endOfDirectory(thisPlugin)
 
 
-# Step 3 - run the program
-sendToXbmc()
 
+params = parameters_string_to_dict(sys.argv[2])
+action = urllib.unquote_plus(params.get('action', ''))
+id = urllib.unquote_plus(params.get('id', ''))
+
+if action=='launch':
+        launchGame(id)
+else:
+        showAllGames()
